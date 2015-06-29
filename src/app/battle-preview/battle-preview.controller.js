88c84a9e13d6e3dd5725186b5029c73026b058f4
Paco Gomez Granados <pacogomezgranados@gmail.com>
2015-06-29 12:04:42 +0200

    Showing players in map preview

--- src/app/battle-preview/battle-preview.controller.js
+++ src/app/battle-preview/battle-preview.controller.js
@@ -2,17 +2,19 @@
     'use strict';
 
     angular.module('caveBattles.battle-preview.controller', [
-        'caveBattles.battle-model'
+        'caveBattles.battle-model',
+        'ngLodash'
     ])
         .controller('BattlePreviewController', BattlePreviewController);
 
-    function BattlePreviewController($scope, BattleModel) {
+    function BattlePreviewController($scope, lodash, BattleModel) {
         var vm = this;
 
         init();
 
         function init() {
             $scope.$watch('vm.map', mapChanged);
+            $scope.$watch('vm.players', playersChanged);
         }
 
         function mapChanged(newMap) {
@@ -21,6 +23,18 @@
                 BattleModel.init(options);
                 vm.battleInfo = BattleModel.model;
             }
+        }
+
+        function playersChanged(newPlayers) {
+            vm.playersToShow = newPlayers;
+            if(newPlayers) {
+                angular.forEach(vm.playersToShow, fillPlayerPosition);
+            }
+        }
+
+        function fillPlayerPosition(player) {
+            var nodeForPlayer = lodash.findWhere(vm.battleInfo.nodes, {id: player.initialNode});
+            player.position = angular.copy(nodeForPlayer.position);
         }
     }
 
